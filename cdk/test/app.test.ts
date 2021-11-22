beeb2a32261d2f5d2312ec5e7d9f4e04e0cf67d6
Adam Quigley <adam.quigley@99designs.com>
2021-11-23 10:37:51 +1100

    Fix CDK test synth

--- cdk/test/app.test.ts
+++ cdk/test/app.test.ts
@@ -3,10 +3,14 @@ import { RustApiStack } from '../lib/api';
 
 describe('RustApiStack', () => {
     test('synthesizes correctly', () => {
-        const app = new cdk.App();
+        const app = new cdk.App({
+            outdir: "./cdk.out",
+        });
 
         new RustApiStack(app, 'RustApiStack', {
             lambdaCodeDistPath: 'test/testdata/lambda.zip',
         });
+
+        app.synth();
     });
 });
